47d09927b33abc428ac05c430f13da2d9823b137
phucle297 <somdp2907@gmail.com>
2022-02-26 21:02:47 +0700

    fix user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -24,7 +24,7 @@ usersRoutes.get(
   authorize("Admin"),
   userController.getById
 );
-usersRoutes.get("get-my-data", authenticate, userController.getMyData);
+usersRoutes.get("/get-my-data", authenticate, userController.getMyData);
 usersRoutes.put("/edit", authenticate, userController.editUser);
 usersRoutes.delete(
   "/delete/:userId",
