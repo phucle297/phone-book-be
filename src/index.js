a8b6776d7f04b7cedf3035e27f99370dd22b86ac
phucle297 <somdp2907@gmail.com>
2022-03-14 18:04:39 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const reponseInterceptor = require("./middleware/interceptors");
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
 app.use(reponseInterceptor);
+
 app.use("/api", rootRouter);
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-app.listen(8080, () => {});
+// app.listen(8080, () => {});
+app.listen(8443, () => {});
