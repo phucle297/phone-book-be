586ac7a7de6158565a58fc8b6d11bf102808cb6d
phucle297 <somdp2907@gmail.com>
2022-03-18 00:34:08 +0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ rm -rf phone-book-be
 git clone https://github.com/phucle297/phone-book-be.git
 cp .env phone-book-be
 cp phone-book.pem phone-book-be/src/models/
+cp ca_bundle.crt phone-book-be/src/
+cp certificate.crt phone-book-be/src/
+cp private.key phone-book-be/src/
 cd phone-book-be
 npm install
 killall screen
